062edbb6b8d627198520eebcad47788e4312debb
Emily G <aspentree79@gmail.com>
2019-09-03 19:55:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,6 +46,6 @@ def checkout(array, coupons)
 hash_cart = consolidate_cart(array)
 applied_coupons = apply_coupons(hash_cart, coupons)
 applied_discount = apply_clearance(applied_coupons)
-
+total = applied_discount.reduce(0) { | acc, (key, value) |  }
 
 end
